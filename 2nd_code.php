5fdf060437eb80c475d1e6c70a60f11aa5cbafc1
rashedmojammel <rashedmojamme56@gmail.com>
2026-01-03 11:28:56 +0600

    Writing php code inside html

---
+++ 2nd_code.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=<device-width>, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <h2>Writing php code inside html</h2>
+    <?php 
+
+    echo "Hello my name is rashedul alam";
+    echo "i am writing php code inside html";
+    ?>
+    
+</body>
+</html>
