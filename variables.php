f3bee79fbf1654141e2fd6b49931290267dc5c39
rashedmojammel <rashedmojamme56@gmail.com>
2026-01-03 12:44:52 +0600

    Basic syntax

--- variables.php
+++ variables.php
@@ -1,7 +1,17 @@
-<?php
-$name = "Rashed";
-$age = 22;
-echo "My name is ".$name;
-echo "<br>";
-echo "My age is " .$age;
-?>
+
+ 
+<?php 
+    $first = "Hello"; 
+    $second = "World"; 
+    $third = "My name is ";
+    $fourth = "Rashedul alam";
+    echo $first . " " . $second; 
+    echo "<br>";
+    echo $third. " " . $fourth; 
+     
+    echo "<br>"; 
+    $text = "PHP"; 
+    $text .= " is awesome!"; 
+    echo $text; 
+?> 
+
