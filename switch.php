f73ce70a2b6bc3e34156cb7d0795307bfb030808
rashedmojammel <rashedmojamme56@gmail.com>
2026-01-04 21:12:19 +0600

    Switch case

---
+++ switch.php
@@ -0,0 +1,18 @@
+<?php 
+
+$day = "Friday";
+switch($day){
+    case "Monday";
+    echo "Start of the week";
+    break;
+    case "Thursday";
+    echo "End of the week";
+    break;
+    case "Friday";
+    echo "Weekend";
+    break;
+    default:
+    echo "Normal day";
+}
+
+?>
