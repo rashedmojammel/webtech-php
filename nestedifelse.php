227784e4b07b009285c2c23183e3107269e173bf
rashedmojammel <rashedmojamme56@gmail.com>
2026-01-04 21:07:49 +0600

    Upadated nested ifele

--- nestedifelse.php
+++ nestedifelse.php
@@ -11,3 +11,25 @@ if ($age >= 18) {
     echo "You are not old enough to vote."; 
 } 
 ?>
+
+<?php 
+
+$age = 20;
+$nationality = true;
+
+if ($age <18){
+    if($nationality){
+        echo "You are nationality but under 18 ";
+    }
+    else {
+       
+        echo " you are not nationality";
+    }
+}
+else {
+    echo "You are eligible to vote";
+
+}
+    
+
+?>
